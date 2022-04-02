5dace6905cac4eabfb5edce4d8047e1bc9cb7507
korolvd <ffirin@mail.ru>
2022-04-02 23:54:43 +0500

    1. ToMany [#301848]

---
+++ src/main/java/HbmRun.java
@@ -0,0 +1,41 @@
+import model.Brand;
+import model.Model;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HbmRun {
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+
+            Model kalina = Model.of("Kalina");
+            session.save(kalina);
+            Model granta = Model.of("Granta");
+            session.save(granta);
+            Model priora = Model.of("Priora");
+            session.save(priora);
+            Model xRay = Model.of("xRay");
+            session.save(xRay);
+            Model vesta = Model.of("Vesta");
+            session.save(vesta);
+
+            Brand lada = Brand.of("Lada");
+            lada.addModel(session.load(Model.class, 1));
+            session.save(lada);
+
+            session.getTransaction().commit();
+            session.close();
+        }  catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
---
+++ src/main/java/model/Model.java
@@ -0,0 +1,37 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "model")
+public class Model {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    public Model() {
+    }
+
+    public static Model of(String name) {
+        Model model = new Model();
+        model.name = name;
+        return model;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
